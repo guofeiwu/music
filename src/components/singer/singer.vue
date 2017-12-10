c64c9f48b04a7aa9e1d7c423fc0b37f42949b5f7
guofei_wu <guofei_wu@163.com>
2017-12-10 22:21:27 +0800

    初步完成歌手界面

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="singer" ref="singer">
-    singer
+    <list-view :data="singerList"></list-view>
   </div>
 </template>
 
 <script>
+  import ListView from 'base/listview/listview'
+  import Singer from 'common/js/singer'
   import { getSingerList } from 'api/singer'
   import { ERR_OK } from 'api/config'
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
   export default {
     data() {
       return {
@@ -20,10 +24,60 @@
       _getSingerList() {
         getSingerList().then(res => {
           if (res.code === ERR_OK) {
-            console.log(res.data)
+            this.singerList = this._formatData(res.data.list)
           }
         })
+      },
+      // 处理数据
+      _formatData(data) {
+        // 定义一个map
+        const map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        // forEach是针对数组的方法
+        data.forEach((item, index) => {
+          // 遍历数据，前10条为热门
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          // 看map对象中是否存在 对应的key
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了等到有序的map， 处理map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
