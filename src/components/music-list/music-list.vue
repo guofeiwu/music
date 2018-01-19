967e048b75f93d6c0ae5a076abe02f6282446c39
wuguofei <guofei_wu@163.com>
2018-01-19 11:17:47 +0800

    歌手详细列表完成

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,6 +5,12 @@
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper" ref="playBtn">
+        <div class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -24,8 +30,10 @@
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
   import Loading from 'base/loading/loading'
+  import {prefixStyle} from 'common/js/dom'
 
   const BANNER_HEIGHT = 40
+  const transform = prefixStyle('transform')
   export default {
     mounted() {
       // 设置列表距离顶部的距离
@@ -78,26 +86,26 @@
         let zIndex = 0
         let scale = 1
         let translateY = Math.max(this.minImageHeight, newY)
-        this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-        this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
         let percent = Math.abs(newY / this.imageHeight)
+        // 向下拉
         if (newY > 0) {
           zIndex = 10
           scale = scale + percent
-        } else {
-
         }
         if (newY < this.minImageHeight) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${BANNER_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
-        this.$refs.bgImage.style['transform'] = `scale(${scale})`
-        this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+        // 设置缩放
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     methods: {
